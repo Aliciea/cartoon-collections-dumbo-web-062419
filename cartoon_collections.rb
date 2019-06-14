dd3320a83a5b3ba7bf0c10ac08b26ae039a2102b
Aliciea D Suechin <adr330@nyu.edu>
2019-06-14 18:19:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
   array = ["Doc", "Dopey", "Bashful", "Grumpy"]
-  array.each { |dwarves| print "#{dwarves}!"
+  puts array.each { |dwarves| print "#{dwarves}!"
   # Your code here
 end
 
